eeca7cc252b76518c8bd5796ce7663533dccaa14
Nataliya Kim <magistr1982@gmail.com>
2023-08-08 14:27:21 +0300

    Add status 200 success test

--- controllers/auth/signin.test.js
+++ controllers/auth/signin.test.js
@@ -1,48 +1,50 @@
-import express from "express";
+// import express from "express";
+// import jwt from "jsonwebtoken";
+// import { authLoginController } from "../auth/index.js";
 import request from "supertest";
 import mongoose from "mongoose";
 import "dotenv/config";
-import { authLoginController } from "../auth/index.js";
+import User from "../../models/user.js";
+import app from "../../app.js";
 
-const { DB_HOST } = process.env;
+const { DB_HOST, PORT } = process.env;
 
-const app = express();
-app.use(express.json());
-app.post("/api/auth/signin", authLoginController.signin);
+const signinData = {
+  email: "kim@gmail.com",
+  password: "123456",
+  subscription: "starter",
+};
 
 // describe the object which would be tested
 describe("test signin controller", () => {
+  let server = null;
   // function launch before all tests
-  beforeAll(async () => await mongoose.connect(DB_HOST));
+  beforeAll(async () => {
+    await mongoose.connect(DB_HOST);
+    server = app.listen(PORT);
+  });
+
   // function launch after all tests
-  afterAll(async () => await mongoose.connection.close());
-
-  test("signin return response status 200", async () => {
-    //   checking response status 200
-    const response = await request(app).post("/api/auth/signin");
-    expect(response.status).toBe(200);
+  afterAll(async () => {
+    await mongoose.connection.close();
+    server.close();
+  });
+  // after each test to delete user from database
+  afterEach(async () => {
+    await User.deleteMany({});
   });
 
-  test("signin returns a non-empty object", async () => {
-    const response = await request(app).post("/api/auth/signin");
-    // Check if the response body is an object
-    expect(typeof response.body).toBe("object");
-    // Check if the object has at least one property
-    expect(Object.keys(response.body).length).toBeGreaterThan(0);
+  test("test signin", async () => {
+    const { status, body, token } = await request(app)
+      .post("/api/auth/signin")
+      .send(signinData);
+    expect(status).toBe(200);
   });
 
-  test("signin object return two strings email and subscription", async () => {
-    const response = await request(app).post("/api/auth/signin");
-    //   checking the response object contains two strings email and subscription
-    const { user } = response.body;
-    expect(typeof user.email).toBe("string");
-    expect(typeof user.subscription).toBe("string");
-  });
+  // expect(body.email).toBe("string");
+  // expect(body.subscription).toBe("string");
+  // expect(token).toBe("string");
 
-  test("signin object return token", async () => {
-    const response = await request(app).post("/api/auth/signin");
-    //   checking the response object contains the string token
-    const { token } = response.body;
-    expect(typeof token).toBe("string");
-  });
+  // const user = await User.findOne({ email: signinData.email });
+  // expect(user.email).toBe(signinData.email);
 });
